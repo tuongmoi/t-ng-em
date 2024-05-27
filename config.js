b7ef6b4ddfaa23a5a01433552c041fabbaae510d
tuongle04 <155515577+tuongmoi@users.noreply.github.com>
2024-05-28 01:46:21 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,17 @@
+const CONFIG = {
+    introTitle: 'Babe à!',
+    introDesc: `Chuyện là tối qua anh nhớ em vl.
+    Nên là anh cóa vài câu hỏi muốn hỏi em`,
+    btnIntro: 'hihi :>',
+    title: 'Anh nhớ em vcl nên là',
+    desc: 'Mình có thể nào gặp nhau được không em ',
+    btnYes: 'Cóa lúc nào cũng được <33',
+    btnNo: 'Méo, gặp cái chóa gì :3',
+    question:'Tối nay anh qua với em nhaa',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    messDesc: 'Tối nay anh qua đón nhé công chúa.',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://www.facebook.com/profile.php?id=100030666693166' 
+}
